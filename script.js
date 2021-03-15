5c97b78275318d40b75b52ed36418dda5089c01d
Patrick Gousseau <pgousse@cs.students.ubc.ca>
2021-03-14 22:25:07 -0500

    added table

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function createBarGraph() {
 	  title: '',
 	  yaxis: {title: 'Particle Count'},
 	  xaxis: {title: 'Size (μm)'},
+	  autosize: true,
 
 	  annotations: [
 	    {
@@ -44,7 +45,7 @@ function createBarGraph() {
 	 
 	};
 	var config = {responsive: true}
-	Plotly.newPlot('barGraph', data, layout, config);
+	Plotly.newPlot('barGraph',data, layout, config);
 }
 
 function createDonut() {
@@ -68,12 +69,13 @@ function createDonut() {
 
 				var layout = {
 				  title: 'Total: 2,934',
+				  autosize: true,
 
 				  annotations: [
 				    {
 				      font: {
 				        size: 20,
-				        
+
 				      },
 				      showarrow: false,
 				      text: '',
@@ -98,4 +100,76 @@ function createDonut() {
 				};
 				var config = {responsive: true}
 				Plotly.newPlot('pieChart', data, layout, config);
+}
+
+window.onresize = function() {
+    Plotly.relayout('pieChart', {
+        'xaxis.autorange': true,
+        'yaxis.autorange': true
+    });
+};
+
+function createTable() {
+	var values = [
+      ['MP_001', 'MP_002','MP_003','MP_004','MP_005','MP_006','MP_007','MP_008','MP_009','MP_010','MP_011'],
+      [150, 170,140,30,170,120,500,609,345,65,34],
+      ['Blue', 'Clear', 'Red', 'Clear', 'Blue','Green','Blue','Blue','Clear','Green','Blue'],
+      ['Fragment','Fiber','Fragment','Fragment','Fiber','Fragment','Fiber','Fragment','Fiber','Fiber','Fragment'],
+		[150.6, 17.4,10.8,300.4,170,120,50.6,60.9,3.45,65,3.4],
+		[15.3, 1.74,108.3,30.2,170,120,50.6,60.5,3.45,65,3.4]]
+
+var data = [{
+  type: 'table',
+  header: {
+    values: [["<b>ID</b>"], ["<b>Size Frac. (μm)</b>"],
+				 ["<b>Colour</b>"], ["<b>Category</b>"], ["<b>Length (μm)</b>"], ["<b>Width (μm)</b>"]],
+    align: "center",
+    fill: {color: '#fbfbfb'},
+    line: {color: "fbfbfb", width: 1},
+    font: {family: "Open Sans", size: 16, color: "black"}
+  },
+  cells: {
+    values: values,
+    height:40,
+    line: {color: "#fbfbfb", width: 1},
+    align: "center",
+    color : "#aeaeae",
+    fill: {color: '#fbfbfb'},
+    font: {family: "Open Sans", size: 16, color: ["#a19a99"]}
+  }
+}]
+
+				var layout = {
+				  title: '',
+				  autosize: true,
+
+				  annotations: [
+				    {
+				      font: {
+				        size: 20,
+
+				      },
+				      showarrow: false,
+				      text: '',
+				      x: 0.2,
+				      y: 0.5
+				    }],
+				  height: 355,
+				  width: 1630,
+				    margin: {
+					    l: 0,
+					    r: 0,
+					    b: 0,
+					    t: 0,
+					    
+					  },
+				  showlegend: true,
+				  legend: {"orientation": "h"},
+				    paper_bgcolor: 'rgba(0,0,0,0)',
+  					plot_bgcolor: 'rgba(0,0,0,0)',
+
+				 
+				};
+
+Plotly.newPlot('table', data, layout);
 }
